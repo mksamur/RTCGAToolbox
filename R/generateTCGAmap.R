eed328bef2deb03288ca40006a6ac5cf6deb628c
LiNk-NY <mramos09@gmail.com>
2016-03-03 18:57:12 -0500

    update documentation and add helper functions for MultiAssayExperiments

---
+++ R/generateTCGAmap.R
@@ -0,0 +1,36 @@
+#' Create a sampleMap from an experiment list and phenoData dataframe
+#' 
+#' This function helps create a sampleMap in preparation of a
+#' \code{MultiAssayExperiment} object
+#' 
+#' @param exlist A named \code{list} of experiments compatible with the 
+#' MultiAssayExperiment API
+#' @param mPheno A \code{data.frame} of clinical data with patient identifiers
+#' as rownames
+#' @return A \code{DataFrame} class object of mapped samples and patient
+#' identifiers including assays
+#' 
+#' @export generateTCGAmap
+generateTCGAmap <- function(exlist, mPheno) {
+    exlist <- MultiAssayExperiment::Elist(exlist)
+    samps <- lapply(exlist, colnames)
+    listM <- lapply(seq_along(samps), function(i, x) {
+        S4Vectors::DataFrame(assay = x[[i]], assayname = Rle(names(x)[i]))
+    }, x = samps)
+    full_map <- do.call(S4Vectors::rbind, listM)
+    # matches <- match(full_map$assay, rownames(mPheno))
+    matches <- match(RTCGAToolbox::bcIDR(full_map$assay), rownames(mPheno))
+    if (all(is.na(matches))) {
+        stop("no way to map pData to Elist")
+    }
+    master <- Rle(rownames(mPheno)[matches])  
+    autoMap <- S4Vectors::cbind(DataFrame(master), full_map)
+    if (any(is.na(autoMap$master))) {
+        notFound <- autoMap[is.na(autoMap$master), ]
+        warning("Data from rows:",
+                sprintf("\n %s - %s", notFound[, 2], notFound[, 3]),
+                "\ndropped due to missing phenotype data")
+    }
+    autoMap <- autoMap[!is.na(autoMap$master), ]
+    return(autoMap)
+}
