f841b8b21e582ac4d212271fffed93e775cc2a79
LiNk-NY <mramos09@gmail.com>
2016-04-08 18:27:53 -0400

    extract changes

--- R/extract.R
+++ R/extract.R
@@ -21,10 +21,13 @@
 #'
 #' @param object A \code{FirehoseData} object from which to extract data.
 #' @param type The type of data to extract from the "FirehoseData" object.
-#' @return Either an \code{\link{ExpressionSet} object,
-#' \code{\link{GRangesList}}) object, or data frame (when no clinical data requested for the selected data type or when clinical data is exclusively requested).
-#' Choices include: "RNAseq_Gene", "Clinic", "miRNASeq_Gene", "RNAseq2_Gene_Norm", "CNA_SNP", "CNV_SNP", "CNA_Seq", "CNA_CGH", "Methylation", "Mutation", "mRNA_Array", "miRNA_Array", "RPPA", "GISTIC_A", "GISTIC_T".
-#' The "GISTIC_A" type of dataset represents GISTIC data by all genes. "GISTIC_T"" represents data thresholded by genes.
+#' @return Either an \code{\link{ExpressionSet} object or a
+#' \code{\link{GRangesList}}) object. Choices include: "RNAseq_Gene",
+#' "Clinic", "miRNASeq_Gene", "RNAseq2_Gene_Norm", "CNA_SNP", "CNV_SNP",
+#' "CNA_Seq", "CNA_CGH", "Methylation", "Mutation", "mRNA_Array",
+#' "miRNA_Array", "RPPA", "GISTIC_A", "GISTIC_T". The "GISTIC_A" type of
+#' dataset represents GISTIC data by all genes. "GISTIC_T" represents data
+#' thresholded by genes.
 #'
 #' @author Marcel Ramos \email{mramos09@@gmail.com}
 #'
@@ -36,117 +39,113 @@
 #'
 #' @export extract
 extract <- function(object, type = NULL) {
-  if (!is.null(type)) {
-    if (is.character(type)) {
-      type <- tolower(gsub("_", "", type))
-    } else {
-      stop("Data type must be a character string")
-    }
-  } else {
-    stop("Specify type")
-  }
-  choices <- tolower(
-    gsub("_", "",
-         c("RNAseq_Gene", "miRNASeq_Gene", "RNAseq2_Gene_Norm",
-           "CNA_SNP", "CNV_SNP", "CNA_Seq", "CNA_CGH", "Methylation",
-           "Mutation", "mRNA_Array", "miRNA_Array", "RPPA")))
-  rangeslots <- c("CNVSNP", "CNASNP", "CNAseq", "CNACGH", "Mutations")
-  if (type %in% choices) {
-    slotreq <- grep(paste0("^", type) , slotNames(object),
-                    ignore.case=TRUE, perl=TRUE, value=TRUE)
-    if (is(getElement(object, slotreq), "list")) {
-      elemlength <- length(getElement(object, slotreq))
-      if (elemlength > 1L) {
-        if (interactive()) {
-          sourceName <- sapply(getElement(object, slotreq),
-                               function(FHarray) {
-                                 getElement(FHarray, "Filename")
-                               })
-          dimensions <- sapply(lapply(getElement(object, slotreq),
-                                      function(tmp) {
-                                        getElement(tmp, "DataMatrix")
-                                      }), dim)
-          cat(paste0("[", seq(length(sourceName)), "] ",
-                     sourceName, paste0("\n\tNumber of rows: ",
-                                        dimensions[1,],
-                                        "\tNumber of columns: ",
-                                        dimensions[2,]) ),
-              fill = TRUE, sep = "\n")
-          fileNo <- .fileSelect()
-          if (fileNo == 0) {
-            fileNo <- which.max(sapply(
-              lapply(getElement(object, slotreq),
-                     function(tmp) {
-                       getElement(tmp, "DataMatrix")
-                     }), ncol)
-            )
-          }
-          message("Selecting file: [", fileNo, "] ", sourceName[fileNo])
-          dm <- getElement(object, slotreq)[[fileNo]]@DataMatrix
+    if (!is.null(type)) {
+        if (is.character(type)) {
+            type <- tolower(gsub("_", "", type))
         } else {
-          dm <- lapply(getElement(object, slotreq),
-                       function(tmp) {
-                         getElement(tmp, "DataMatrix")
-                       })
-          keeplist <- which.max(sapply(dm, ncol))
-          dm <- dm[[keeplist]]
-          warning(paste("Taking the array platform with",
-                        "the greatest number of samples:", keeplist))
+            stop("Data type must be a character string")
         }
-      } else if(elemlength == 1L) {
-        dm <- getElement(object, slotreq)[[1]]@DataMatrix
-      } else if(elemlength == 0L) {
-        dm <- matrix(NA, nrow=0, ncol=0)
-      }
     } else {
-      dm <- getElement(object, slotreq)
+        stop("Specify type")
     }
-  } else  if (type %in% c("gistica", "gistict")) {
-    if(type=="gistica"){
-      slotreq <- "AllByGene"
+    choices <- tolower(
+        gsub("_", "",
+             c("RNAseq_Gene", "miRNASeq_Gene", "RNAseq2_Gene_Norm",
+               "CNA_SNP", "CNV_SNP", "CNA_Seq", "CNA_CGH", "Methylation",
+               "Mutation", "mRNA_Array", "miRNA_Array", "RPPA")))
+    rangeslots <- c("CNVSNP", "CNASNP", "CNAseq", "CNACGH", "Mutations")
+    if (type %in% choices) {
+        slotreq <- grep(paste0("^", type) , slotNames(object),
+                        ignore.case=TRUE, perl=TRUE, value=TRUE)
+        if (is(getElement(object, slotreq), "list")) {
+            elemlength <- length(getElement(object, slotreq))
+            if (elemlength > 1L) {
+                if (interactive()) {
+                    sourceName <- sapply(getElement(object, slotreq),
+                                         function(FHarray) {
+                                             getElement(FHarray, "Filename")
+                                         })
+                    dimensions <- sapply(lapply(getElement(object, slotreq),
+                                                function(tmp) {
+                                                    getElement(tmp, "DataMatrix")
+                                                }), dim)
+                    cat(paste0("[", seq(length(sourceName)), "] ",
+                               sourceName, paste0("\n\tNumber of rows: ",
+                                                  dimensions[1,],
+                                                  "\tNumber of columns: ",
+                                                  dimensions[2,]) ),
+                        fill = TRUE, sep = "\n")
+                    fileNo <- .fileSelect()
+                    if (fileNo == 0) {
+                        fileNo <- which.max(sapply(
+                            lapply(getElement(object, slotreq),
+                                   function(tmp) {
+                                       getElement(tmp, "DataMatrix")
+                                   }), ncol)
+                        )
+                    }
+                    message("Selecting file: [", fileNo, "] ", sourceName[fileNo])
+                    dm <- getElement(object, slotreq)[[fileNo]]@DataMatrix
+                } else {
+                    dm <- lapply(getElement(object, slotreq),
+                                 function(tmp) {
+                                     getElement(tmp, "DataMatrix")
+                                 })
+                    keeplist <- which.max(sapply(dm, ncol))
+                    dm <- dm[[keeplist]]
+                    warning(paste("Taking the array platform with",
+                                  "the greatest number of samples:", keeplist))
+                }
+            } else if(elemlength == 1L) {
+                dm <- getElement(object, slotreq)[[1]]@DataMatrix
+            } else if(elemlength == 0L) {
+                dm <- matrix(NA, nrow=0, ncol=0)
+            }
+        } else {
+            dm <- getElement(object, slotreq)
+        }
+    } else  if (type %in% c("gistica", "gistict")) {
+        if(type=="gistica"){
+            slotreq <- "AllByGene"
+        } else {
+            slotreq <- "ThresholdedByGene"
+        }
+        dm <- getElement(object@GISTIC, slotreq)
     } else {
-      slotreq <- "ThresholdedByGene"
+        stop(paste("Data type not yet supported or could not be matched."))
     }
-    dm <- getElement(object@GISTIC, slotreq)
-  } else {
-    stop(paste("Data type not yet supported or could not be matched."))
-  }
-  if (dim(dm)[1] == 0 | dim(dm)[2] == 0) {
-    stop("There is no data for that data type!")
-  } else {
-    if (slotreq %in% c("Methylation", "AllByGene", "ThresholdedByGene")) {
-      annote <- dm[, !grepl("TCGA", names(dm))]
-      isCharRow <- all(grepl("[0-9]", rownames(dm)))
-      if (isCharRow) {
-        geneSymbols <- annote[, grep("symbol", names(annote),
-                                     ignore.case = TRUE, value = TRUE)]
-        rNames <- geneSymbols
-        rNames <- ifelse(grepl("|", rNames, fixed = TRUE),
-                                strsplit(rNames, "\\|")[[1]][1], rNames)
-      } else {
-      rNames <- rownames(dm)
-      }
-      dm <- apply(dm[grepl("TCGA", names(dm))], 2, as.numeric, as.matrix)
-      rownames(dm) <- rNames
-      filler <- substr(colnames(dm)[1], 5, 5)
-      if (filler != "-") {
-        colnames(dm) <- gsub(paste0("\\", filler), "-", colnames(dm))
-      }
-      return(dm)
-    } else if (slotreq %in% rangeslots) {
-      colnames(dm) <- tolower(colnames(dm))
-      mygrl <- makeGRangesList(dm)
-      if(exists("sourceName")) {
-        mygrl@metadata <- list("fileName" = sourceName[fileNo])
-      }
-      return(mygrl)
+    if (dim(dm)[1] == 0 | dim(dm)[2] == 0) {
+        stop("There is no data for that data type!")
     } else {
-      colnames(dm) <- bcIDR(colnames(dm), sample=TRUE, collapse=TRUE)
-      eset <- ExpressionSet(dm)
-      if (exists("annote")) {
-        featureData(eset) <- AnnotatedDataFrame(annote)
-      }
-      return(eset)
+        if (slotreq %in% c("Methylation", "AllByGene", "ThresholdedByGene")) {
+            annote <- dm[, !grepl("TCGA", names(dm))]
+            isCharRow <- all(grepl("[0-9]", rownames(dm)))
+            if (isCharRow) {
+                geneSymbols <- annote[, grep("symbol", names(annote),
+                                             ignore.case = TRUE, value = TRUE)]
+                rNames <- geneSymbols
+            } else {
+                rNames <- rownames(dm)
+            }
+            dm <- apply(dm[grepl("TCGA", names(dm))], 2, as.numeric, as.matrix)
+            rownames(dm) <- rNames
+            filler <- substr(colnames(dm)[1], 5, 5)
+            if (filler != "-") {
+                colnames(dm) <- gsub(paste0("\\", filler), "-", colnames(dm))
+            }
+        } else if (slotreq %in% rangeslots) {
+            colnames(dm) <- tolower(colnames(dm))
+            mygrl <- makeGRangesList(dm)
+            if(exists("sourceName")) {
+                mygrl@metadata <- list("fileName" = sourceName[fileNo])
+            }
+            return(mygrl)
+        } 
+        colnames(dm) <- bcIDR(colnames(dm), sample=TRUE, collapse=TRUE)
+        eset <- ExpressionSet(dm)
+        if (exists("annote")) {
+            featureData(eset) <- AnnotatedDataFrame(annote)
+        }
+        return(eset)
     }
-  }
 }
