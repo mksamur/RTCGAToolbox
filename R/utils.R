4b73884b1fa7d3ef07c25aedf87c7da63a1d2a96
LiNk-NY <mramos09@gmail.com>
2017-12-27 20:21:50 -0500

    update unexported supporting functions

--- R/utils.R
+++ R/utils.R
@@ -249,7 +249,7 @@
 ## Safe to assume equal number of ranges == equal ranges (?)
 
 .makeRangedSummarizedExperimentFromDataFrame <- function(df, ...,
-                                                         seqinfo = NULL, starts.in.df.are.0based = FALSE) {
+    seqinfo = NULL, starts.in.df.are.0based = FALSE) {
     args <- list(...)
     if (!is.null(args[["build"]]))
         GBuild <- args[["build"]]
@@ -327,7 +327,28 @@
     return(object)
 }
 
-## Helper functions
+## Helper functions from TCGAutils (to be released) see findGRangesCols
+.find_with_xfix <- function(df_colnames, xfix1, xfix2,
+                            start.field, end.field, xfixType = "pre") {
+    fixint <- intersect(xfix1, xfix2)
+    fixint <- fixint[fixint != ""]
+    if (!S4Vectors::isSingleString(fixint))
+        stop("'start.field' and 'end.field' ", xfixType, "fixes do not match")
+    names(fixint) <- xfixType
+
+    fixFUN <- switch(xfixType, pre = I, suf = rev)
+    start.field <- paste(fixFUN(c(fixint, start.field)), collapse = "")
+    validEnd <- vapply(end.field, function(efield)
+        paste(fixFUN(c(fixint, efield)), collapse = "") %in% df_colnames,
+        logical(1L))
+    stopifnot(sum(validEnd) == 1L)
+    end.field <- paste(fixFUN(c(fixint, end.field[validEnd])), collapse = "")
+    if (!length(start.field) && !length(end.field))
+        list(c(start.field = "", end.field = ""), "")
+    else
+        list(c(start.field = start.field, end.field = end.field), fixint)
+}
+
 .find_start_end_cols <- function (df_colnames, start.field, end.field) {
     idx1 <- which(df_colnames %in% start.field)
     idx2 <- which(df_colnames %in% end.field)
@@ -335,73 +356,34 @@
     prefixes2 <- .collect_prefixes(df_colnames, end.field)
     suffixes1 <- .collect_suffixes(df_colnames, start.field)
     suffixes2 <- .collect_suffixes(df_colnames, end.field)
-    if (length(idx1) != 1L || length(idx2) != 1L) {
-        matchPre <- intersect(prefixes1, prefixes2)
-        stopifnot(S4Vectors::isSingleString(matchPre))
-        if (length(nchar(matchPre))) {
-            start.field <- start.field[grep(paste0("^", matchPre), start.field)]
-            end.field <- end.field[grep(paste0("^", matchPre), end.field)]
-        }
-        matchSuf <- intersect(suffixes1, suffixes2)
-        stopifnot(S4Vectors::isSingleString(matchSuf))
-        if (length(nchar(matchSuf))) {
-            start.field <- start.field[grep(paste0(matchSuf, "$"), start.field)]
-            end.field <- end.field[grep(paste0(matchSuf, "$"), end.field)]
-        }
-        idx1 <- which(df_colnames %in% start.field)
-        idx2 <- which(df_colnames %in% end.field)
-    } else if (!length(idx1) && !length(idx2)) {
-        if (length(prefixes1) && length(prefixes2)) {
-            if (length(prefixes1) >= 2L && length(prefixes2) >= 2L) {
-                warning("multiple prefixes found, using first match")
-                if (prefixes1[[1L]] == prefixes2[[1L]])
-                    prefix <- prefixes1[[1L]]
-            } else if (length(prefixes1) && length(prefixes2) &&
-                       prefixes1 == prefixes2) {
-                prefix <- prefixes1
-            }
-            idx1 <- which(df_colnames %in% paste0(prefix, start.field))
-            idx2 <- which(df_colnames %in% paste0(prefix, end.field))
-            if (length(idx1) == 1L && length(idx2) == 1L) {
-                return(list(c(start = idx1, end = idx2), prefix)) }
-        } else if (length(suffixes1) && length(suffixes2)) {
-            if (length(suffixes1) >= 2L && length(suffixes2) >= 2L) {
-                warning("multiple suffixes found, using first match")
-                if (suffixes1[[1L]] == suffixes2[[1L]])
-                    suffix <- suffixes1[[1L]]
-            } else if (length(suffixes1) == 1L && length(suffixes2) == 1L &&
-                       suffixes1 == suffixes2) {
-                suffix <- suffixes1
-            }
-            idx1 <- which(df_colnames %in% paste0(start.field, suffix))
-            idx2 <- which(df_colnames %in% paste0(end.field, suffix))
-            if (length(idx1) == 1L && length(idx2) == 1L) {
-                return(list(c(start = idx1, end = idx2), "")) }
-        } else {
-            return(list(c(start = NA_integer_, end = NA_integer_), ""))
-        }
+    if (length(idx1) == 1L && length(idx2) == 1L) {
+        return(list(c(start = idx1, end = idx2), ""))
     }
-    return(list(c(start = idx1, end = idx2), ""))
-}
-
-.find_seqnames_col <- function (df_colnames, seqnames.field, prefix) {
-    idx <- which(df_colnames %in% paste0(prefix, seqnames.field))
-    if (length(idx) == 0L)
-        idx <- which(df_colnames %in% seqnames.field)
-    if (length(idx) == 0L)
-        return(NA_integer_)
-    if (length(idx) >= 2L)
-        warning("cannnot determine seqnames column unambiguously")
-    return(idx[[1L]])
-    idx
+    if (length(idx1) != 1L && length(prefixes1) ||
+        length(idx2) != 1L && length(prefixes2)) {
+        startend.fields <- .find_with_xfix(df_colnames, prefixes1, prefixes2,
+            start.field, end.field, "pre")
+        idx1 <- which(df_colnames %in% startend.fields[[1L]][["start.field"]])
+        idx2 <- which(df_colnames %in% startend.fields[[1L]][["end.field"]])
+    }
+    if (!length(idx1) && !length(idx2)) {
+        startend.fields <- .find_with_xfix(df_colnames, suffixes1, suffixes2,
+            start.field, end.field, "suf")
+        idx1 <- which(df_colnames %in% startend.fields[[1L]][["start.field"]])
+        idx2 <- which(df_colnames %in% startend.fields[[1L]][["end.field"]])
+    }
+    if (length(idx1) == 1L && length(idx2) == 1L) {
+        list(c(start = idx1, end = idx2), startend.fields[2L])
+    } else {
+        list(c(start = NA_integer_, end = NA_integer_), "")
+    }
 }
 
 .collect_prefixes <- function (df_colnames, field) {
     df_colnames_nc <- nchar(df_colnames)
     prefixes <- lapply(field, function(suf) {
         pref_nc <- df_colnames_nc - nchar(suf)
-        idx <- which(substr(df_colnames, pref_nc + 1L, df_colnames_nc) ==
-                         suf)
+        idx <- which(substr(df_colnames, pref_nc + 1L, df_colnames_nc) == suf)
         substr(df_colnames[idx], 1L, pref_nc[idx])
     })
     unique(unlist(prefixes))
@@ -416,8 +398,10 @@
     unique(unlist(suffixes))
 }
 
-.find_strands_col <- function(df_colnames, strand.field, prefix) {
-    idx <- which(df_colnames %in% paste0(prefix, strand.field))
+.find_strands_col <- function(df_colnames, strand.field, xfix) {
+    fixFUN <- switch(names(xfix[[1]]), pre = I, suf = rev)
+    idx <- which(df_colnames %in%
+        paste(fixFUN(c(xfix, strand.field)), collapse = ""))
     if (length(idx) == 0L)
         idx <- which(df_colnames %in% strand.field)
     if (length(idx) == 0L)
@@ -429,30 +413,57 @@
     idx
 }
 
+.find_seqnames_col <- function (df_colnames, seqnames.field, xfix) {
+    fixFUN <- switch(names(xfix[[1]]), pre = I, suf = rev)
+    idx <- which(df_colnames %in%
+                     paste(fixFUN(c(xfix, seqnames.field)), collapse = ""))
+    if (length(idx) == 0L)
+        idx <- which(df_colnames %in% seqnames.field)
+    if (length(idx) == 0L)
+        return(NA_integer_)
+    if (length(idx) >= 2L)
+        warning("cannnot determine seqnames column unambiguously")
+    return(idx[[1L]])
+    idx
+}
+
+.find_width_col <- function (df_colnames, width.field, xfix) {
+    fixFUN <- switch(names(xfix[[1]]), pre = I, suf = rev)
+    idx <- which(df_colnames %in%
+        paste(fixFUN(c(xfix, width.field)), collapse = ""))
+    if (length(idx) == 0L)
+        idx <- which(df_colnames %in% width.field)
+    if (length(idx) == 0L)
+        return(NA_integer_)
+    if (length(idx) >= 2L) {
+        warning("cannnot determine width column unambiguously")
+        return(idx[[1L]])
+    }
+    idx
+}
+
 findGRangesCols <- function (df_colnames,
-                             seqnames.field = c("seqnames", "seqname",
-                                                "chromosome", "chrom", "chr",
-                                                "chromosome_name", "seqid"),
-                             start.field = "start",
-                             end.field = c("end", "stop"),
-                             strand.field = "strand",
-                             ignore.strand = FALSE) {
+    seqnames.field = c("seqnames", "seqname", "chromosome",
+        "chrom", "chr", "chromosome_name", "seqid"),
+    start.field = "start",
+    end.field = c("end", "stop"),
+    strand.field = "strand",
+    ignore.strand = FALSE) {
+
     df_colnames0 <- tolower(df_colnames)
     seqnames.field0 <- GenomicRanges:::.normarg_field(seqnames.field, "seqnames")
     start.field0 <- GenomicRanges:::.normarg_field(start.field, "start")
     end.field0 <- GenomicRanges:::.normarg_field(end.field, "end")
     start_end_cols <- .find_start_end_cols(df_colnames0, start.field0,
                                            end.field0)
-    prefix <- start_end_cols[[2L]]
-    width_col <- GenomicRanges:::.find_width_col(df_colnames0, "width", prefix)
-    seqnames_col <- .find_seqnames_col(df_colnames0, seqnames.field0,
-                                       prefix)
+    xfix <- start_end_cols[[2L]]
+    width_col <- .find_width_col(df_colnames0, "width", xfix)
+    seqnames_col <- .find_seqnames_col(df_colnames0, seqnames.field0, xfix)
     if (ignore.strand) {
         strand_col <- NA_integer_
-    }
-    else {
+    } else {
         strand.field0 <- GenomicRanges:::.normarg_field(strand.field, "strand")
-        strand_col <- .find_strands_col(df_colnames0, strand.field0, prefix)
+        strand_col <- .find_strands_col(df_colnames0, strand.field0, xfix)
     }
     c(seqnames = seqnames_col, start_end_cols[[1L]], width = width_col,
       strand = strand_col)
